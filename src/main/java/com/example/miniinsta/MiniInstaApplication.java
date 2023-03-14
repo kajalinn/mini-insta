4792c0793c57772d30b73c2ff16e01239c81cb5e
Nikita Kajalin <kajalinn@tlu.ee>
2023-03-14 14:03:59 +0200

    create spring core

---
+++ src/main/java/com/example/miniinsta/MiniInstaApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniinsta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniInstaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniInstaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/miniinsta/MiniInstaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniinsta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniInstaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
